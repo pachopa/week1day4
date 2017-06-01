7c881b99ac72959e5bd7deade9ca011c57a9298f
Chris Lee <pachopa0320@gmail.com>
2017-06-01 11:18:27 -0700

    inserted forEach function

--- findWaldo.js
+++ findWaldo.js
@@ -1,9 +1,16 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);   // execute callback
+//     }
+//   }
+  arr.forEach(function(item, index, arr) {
+    //console.log(item, index, arr);
+    //console.log(item[index]);
+    if(item === "Waldo") {
+        found(index);
     }
-  }
+  })
 }
 
 function actionWhenFound(i) {

404d52828e7dd847364d4cbf827ec514e14e0d9f
Chris Lee <pachopa0320@gmail.com>
2017-06-01 12:09:12 -0700

    arranged alphabetical order and compared age by descend

---
+++ sortingByname.js
@@ -0,0 +1,48 @@
+
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a,b){
+  if(a.name === b.name){
+    return (a.age + b.age);
+  }
+  else{
+    return +(a.name > b.name);
+  }
+});
+
+students.sort(function(a,b) {
+    var nameA = a.name.toUpperCase();
+    var nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // names must be equal
+  return 0;
+});
+
+console.log(students);
+
+
+// // sort by name
+// items.sort(function(a, b) {
+//   var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+//   var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+//   if (nameA < nameB) {
+//     return -1;
+//   }
+//   if (nameA > nameB) {
+//     return 1;
+//   }
+
+//   // names must be equal
+//   return 0;
+// });
